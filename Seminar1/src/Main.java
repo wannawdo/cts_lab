9b05102a010d565c13e5f0dab5fbc3d64cbb79bb
Oana Dragomirescu <dragomirescuoana18@stud.ase.ro>
2021-02-25 08:59:01 +0200

    seminar1

--- Seminar1/src/Main.java
+++ Seminar1/src/Main.java
@@ -1,10 +1,9 @@
-package ro.ase.cts.seminar1;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Buna buna bunaaa!");
+System.out.println("Buna");
 	}
 
 }
