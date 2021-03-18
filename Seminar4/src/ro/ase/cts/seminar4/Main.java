0b3f2ee3a65dd24bdfec6bc48a2b318fdb1a4c73
Oana Dragomirescu <dragomirescuoana18@stud.ase.ro>
2021-03-18 09:25:39 +0200

    seminar 4 final

---
+++ Seminar4/src/ro/ase/cts/seminar4/Main.java
@@ -0,0 +1,10 @@
+package ro.ase.cts.seminar4;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ShoppingCart myCart = ShoppingCart.getInstance();
+		myCart = ShoppingCart.getInstance();
+	}
+
+}
